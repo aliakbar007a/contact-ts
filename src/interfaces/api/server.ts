4eee20f727a615765914954690455af3f79b8ce0
aliakbar007a <aliakbaramereh370@gmail.com>
2025-11-05 16:25:01 +0330

    ddww

--- src/interfaces/api/server.ts
+++ src/interfaces/api/server.ts
@@ -1,17 +1,8 @@
-import express from "express";
-import contactRoutes from "./routes/contactRoutes";
+import app from "./app"
 
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-
-app.use(express.json());
-
-
-app.use("/api/contacts", contactRoutes);
-
-
-app.listen(PORT, () => {
+export function startServer() {
+  const PORT =  3000;
+  app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+ }
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+import { startServer } from "./interfaces/api/server";
+
+startServer()
